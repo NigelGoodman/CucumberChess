56ed25307952fd7cddf45a71130cccf97ea8d605
7 <den@inazuma.org>
2018-11-19 15:47:45 +0300

    Modified runner to be ale to use JUnit 4 to track tests.
    Please use JUnit 4 to run these tests

--- CucumberChess/src/test/java/runner/testrunner.java
+++ CucumberChess/src/test/java/runner/testrunner.java
@@ -1,15 +1,17 @@
 package runner;
 
-//import org.junit.*;
+import org.junit.*;
 //import org.junit.runner.*;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+// please ensure that you use JUnit4 to run these tests as a suite.
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
-features = "src/test/javaFeatures"
+features = "src/test/java/features"
 ,glue= {"seleniumgluecode"},
 tags= {"@analysis,@import"}
 )
